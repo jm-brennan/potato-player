0e38703d0e2194234956703458deb971761d9e14
Jacob Brennan <bocajnannerb@gmail.com>
2023-05-25 10:31:41 -0400

    render setup

---
+++ src/definitions.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <glad/glad.h>
+
+#define GLEC(x) gl_clear_error();\
+    x;\
+    gl_log_call(#x, __FILE__, __LINE__)
+
+static void gl_clear_error() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool gl_log_call(const char* function, const char* file, int line) {
+    bool error = false;
+    if (GLenum error = glGetError()) {
+        std::cout << "[OpenGL Error] (" << error << ") in file " << file 
+                << " in function " << function << " on line " << line << std::endl;
+        error = true;
+    }
+    return error;
+}
+
+static const float PI = 3.14159f;
+static const float PI_HALF = 1.570795;
+static const float PI_2 = 6.28318f;
--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,146 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include "gpio.h"
-#include "utils.h"
-#include <unistd.h>
-#include <filesystem>
+#include <cstdlib>
+#include <chrono>
 #include <thread>
-#define MINIAUDIO_IMPLEMENTATION
-#include "miniaudio.h"
-#include <stdio.h>
 
-//#include <SFML/Audio.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
-{
-    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
-    if (pDecoder == NULL) {
-        return;
+#include "definitions.h"
+
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+//void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+//void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+void center_window(GLFWwindow* window);
+void main_loop(GLFWwindow* window);
+
+unsigned int SCREEN_WIDTH = 1920;//1140
+unsigned int SCREEN_HEIGHT = 1080;//641
+// globals for the glfw callbacks to access
+
+int main(int argc, char *argv[]) {
+    // Glfw leaks 48,157 bytes
+    if (!glfwInit()) {
+        printf("ERROR::GLFW init\n");
+        return -1;
     }
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount, NULL);
-
-    (void)pInput;
-}
-
-int main(int argc, char** argv) {
-
-    //sf::Music music;
-
-    //std::filesystem::path p ("C:/absolute/path/to/the/audio/file.wav");
-    //assert(std::filesystem::exists(p));
-
-    //music.openFromFile("/home/jacob/src/potato-mp3-player/res/July.mp3");
-    //music.play();
-
-    // Wait for the music to finish, or the program would finish before hearing any sound.
-    //std::this_thread::sleep_for(std::chrono::seconds(10));
-    //return 0;
-
-    ma_result result;
-    ma_decoder decoder;
-    ma_device_config deviceConfig;
-    ma_device device;
-
-    if (argc < 2) {
-        printf("No input file.\n");
+    // @TODO call with glfwGetPrimaryMonitor() to do fullscreen, use cl args?
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Potato Player", nullptr, nullptr);
+    if (!window) {
+        printf("ERROR::GLFW create window. May have invalid GPU drivers.\n");
+        glfwTerminate();
         return -1;
     }
 
-    result = ma_decoder_init_file(argv[1], NULL, &decoder);
-    if (result != MA_SUCCESS) {
-        printf("Could not load file: %s\n", argv[1]);
-        return -2;
+    center_window(window);
+    glfwMakeContextCurrent(window);
+
+    //glfwSetKeyCallback(window, key_callback);
+    //glfwSetMouseButtonCallback(window, mouse_button_callback);
+    //glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    glfwSwapInterval(1);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("ERROR::GLAD init\n");
+        return -1;
     }
 
-    deviceConfig = ma_device_config_init(ma_device_type_playback);
-    deviceConfig.playback.format   = decoder.outputFormat;
-    deviceConfig.playback.channels = decoder.outputChannels;
-    deviceConfig.sampleRate        = decoder.outputSampleRate;
-    deviceConfig.dataCallback      = data_callback;
-    deviceConfig.pUserData         = &decoder;
+    GLEC(glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
+    GLEC(glEnable(GL_BLEND));
+    GLEC(glDisable(GL_DEPTH_TEST));
+    GLEC(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-    if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
-        printf("Failed to open playback device.\n");
-        ma_decoder_uninit(&decoder);
-        return -3;
+    main_loop(window);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
+void main_loop(GLFWwindow* window) {
+    const float FPS = 30.0f;
+    const double FRAME_TIME = 1.0f / FPS;
+    double dt = 0.0f;
+    double currentTime = glfwGetTime();
+    double newTime;
+    double sleepTime;
+
+    // for timing tests
+    //double timeTaken = 0.0;
+    //int numLoops = 0;
+
+    while (!glfwWindowShouldClose(window)) {
+        newTime = glfwGetTime();
+        dt = newTime - currentTime;
+        currentTime = newTime;
+
+        //im->update_mouse_pos();
+        //glfwPollEvents(); // inputmanager receives newKeyDows and newKeyUps
+
+        //game1.process_input((float)dt);
+        //game1.update((float)dt);
+        //im->update_new_inputs(); // resolve newKeyDowns to keyDowns, same for mouse
+
+        GLEC(glClearColor(0.19f, 0.65f, 0.32f, 1.0f));
+        GLEC(glClear(GL_COLOR_BUFFER_BIT));
+        //game1.render();
+
+        GLEC(glfwSwapBuffers(window));
+
+        newTime = glfwGetTime();
+        //timeTaken += (newTime - currentTime);
+        //numLoops++;
+        sleepTime = FRAME_TIME - (newTime - currentTime);
+        if (sleepTime > 0) {
+            std::this_thread::sleep_for(std::chrono::microseconds((int)(sleepTime * 1000000)));
+        }
     }
-
-    if (ma_device_start(&device) != MA_SUCCESS) {
-        printf("Failed to start playback device.\n");
-        ma_device_uninit(&device);
-        ma_decoder_uninit(&decoder);
-        return -4;
+    //printf("%f seconds for %d loops\n", timeTaken, numLoops);
+}
+/* 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (key >= 0 && key <= GLFW_KEY_LAST) {
+        if (action == GLFW_PRESS) {
+            im->set_key_status(key, true);
+        } else if (action == GLFW_RELEASE) {
+            im->set_key_status(key, false);
+        }
     }
+}
 
-    printf("Press Enter to quit...");
-    getchar();
-
-    ma_device_uninit(&device);
-    ma_decoder_uninit(&decoder);
-
-
-   /*  printf("Initializing pin 484\n");
-
-    GPIO_Pin pin;
-    pin.direction = READ;
-    pin.pinNumber = 484;
-
-    printf("exporting\n");
-    Export(pin);
-    printf("exported\n");
-
-    for (int i = 0; i < 10; ++i) {
-        printf("Reading value...");
-        GPIO_PinValue val = Read(pin);
-        printf("\n%d\n", val);
-        sleep(1);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+    if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        im->set_mouse_status(true);
+    } else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+        im->set_mouse_status(false);
     }
+}
 
-    printf("completed, unexporting\n");
-    Unexport(pin); */
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    GLEC(glViewport(0, 0, width, height));
+    game->set_window_size((unsigned int)width, (unsigned int)height);
+} */
+
+void center_window(GLFWwindow *window) {
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+
+    if (!monitor) return;
+
+    const GLFWvidmode *mode = glfwGetVideoMode(monitor);
+    if (!mode) return;
+
+    int monitorX, monitorY;
+    glfwGetMonitorPos(monitor, &monitorX, &monitorY);
+
+    int windowWidth, windowHeight;
+    glfwGetWindowSize(window, &windowWidth, &windowHeight);
+
+    glfwSetWindowPos(window,
+                     monitorX + (mode->width - windowWidth) / 2,
+                     monitorY + (mode->height - windowHeight) / 2);
 }
