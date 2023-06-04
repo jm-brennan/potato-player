d6ea321c3eb9e8aa03fe54a6b4296dd52eb99f30
Jacob Brennan <bocajnannerb@gmail.com>
2023-06-04 11:38:03 -0400

    compile args

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,65 @@
+
+#ifdef USE_OPENGL_ES
 #define GLAD_GLES2_IMPLEMENTATION
 #include <glad/gles2.h>
+#else
+#include <glad/gl.h>
+#endif
+
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
 
 #include <iostream>
 
 using namespace glm;
 using namespace std;
+
+
+using namespace glm;
+
+typedef struct Vertex
+{
+    vec2 pos;
+    vec3 col;
+} Vertex;
+
+static const Vertex vertices[3] =
+{
+    { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
+    { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
+    { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
+};
+
+static const char* vertex_shader_text =
+"#version 100\n"
+"precision mediump float;\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+static const char* fragment_shader_text =
+"#version 100\n"
+"precision mediump float;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
 static void glfw_error_callback(int error, const char* description)
 {
@@ -22,11 +74,20 @@ int main() {
         return 1;
     }
 
+    #ifdef USE_OPENGL_ES
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
-    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL ES 2.0 Triangle (EGL)", NULL, NULL);
+    #else
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #endif
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Potato Player", NULL, NULL);
 
     if (!window) {
         // apparently if window creation fails, it may be worthwhile to retry after
@@ -38,7 +99,70 @@ int main() {
     }
 
     glfwMakeContextCurrent(window);
+    #ifdef USE_OPENGL_ES
     gladLoadGLES2(glfwGetProcAddress);
+    #else
+    gladLoadGL(glfwGetProcAddress);
+    #endif
+
     glfwSwapInterval(1);
+
+    GLuint vertex_buffer;
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+    const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+    const GLuint program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+    const GLint mvp_location = glGetUniformLocation(program, "MVP");
+    const GLint vpos_location = glGetAttribLocation(program, "vPos");
+    const GLint vcol_location = glGetAttribLocation(program, "vCol");
+    glEnableVertexAttribArray(vpos_location);
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex), (void*) offsetof(Vertex, pos));
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex), (void*) offsetof(Vertex, col));
+    while (!glfwWindowShouldClose(window))
+    {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        const float ratio = width / (float) height;
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        //mat4x4 m, p, mvp;
+        mat4 m = mat4( 1.0 );
+        mat4 p = mat4( 1.0 );
+
+        m = rotate(m, (float)glfwGetTime(), vec3(0.0f, 0.0f, 1.0f));
+
+        p = ortho(-1.0f, 1.0f, -1.0f, 1.0f);
+
+        mat4 mvp = p * m;
+
+
+        //mat4x4_identity(m);
+        //mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+        //mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        //mat4x4_mul(mvp, p, m);
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &mvp);
+        
+        glClearColor(0.19f, 0.65f, 0.32f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+
 
 }
